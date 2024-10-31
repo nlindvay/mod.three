f3572b6f6559741855f7d01120d9ba8e00095248
Nicholas Lindvay <Nicholas.Lindvay@logisticsplus.com>
2024-10-31 14:18:13 -0400

    added reference to mod 2 sub mod

--- src/mod.three.lib/ModThreeClass.cs
+++ src/mod.three.lib/ModThreeClass.cs
@@ -1,8 +1,15 @@
-﻿namespace mod.three.lib
+﻿using mod.two.lib;
+
+namespace mod.three.lib
 {
     public class ModThreeClass
     {
+
+        public string GetTwoClassString() => InstanceOfModTwoClass.Blah();
+
         public string GetIt() => "ModThreeClass.GetIt()";
+
+        public ModTwoClass InstanceOfModTwoClass { get; set; }
 
         public string GetThreeClassString()
         {
